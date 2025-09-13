7e9dcb5af78b160e90224c17e4112cb18e5083bf
Abdul Raheem <abdulraheem07.tbox@gmail.com>
2025-09-13 21:47:30 +0500

    Fix component folder/file casing for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import { useState } from 'react'
 import './App.css'
-
-import Hero from './components/Hero/hero'
-import About from './components/about/about'
-import Projects from './components/Projects/project'
-import Contact from './components/Contact/contact'
-import Navbar from "./components/Navbar/Navbar";
-
+import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Hero/Hero';
+import About from './components/About/About';
+import Projects from './components/Projects/Project';
+import Contact from './components/Contact/Contact';
 
 
 
 export default function App() {
   return (
     <div>
-     <Navbar/>
+      <Navbar/>
       <Hero/>
       <About/>
       <Projects/>
